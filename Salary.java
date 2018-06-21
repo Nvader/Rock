2841771812ac3a1d4427fb356926fead71b0616b
oleg.bugayev <oleg.bugayev@globallogic.com>
2018-06-21 14:03:46 +0300

    123test

---
+++ Salary.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SalaryComparator {
+    public static void main(String[] args) {
+        String Junior = "Junior";
+        String Middle = "Middle";
+        String Senior = "Senior";
+
+        System.out.println("Enter your name below");
+
+        String s = new Scanner(System.in).next();
+
+        System.out.println("Your name is: " + s + "; " + "\n" + "welcome to the System" + ", " + s);
+
+        System.out.println("Please add below your age");
+
+        int a = Integer.parseInt(new Scanner(System.in).next());
+
+
+        if (a < 21) {
+            System.out.println("You can't work for our company due to your young age.");
+
+        } else {
+
+            System.out.println("You entered value: " + a + " " + "\n" + "please add below your IT experience in years");
+        }
+
+        int b = Integer.parseInt(new Scanner(System.in).next());
+
+        if (b < 1.5) {
+            System.out.println("Your experience level is " + " " + Junior);
+        }
+        if (b >= 1.5 && b <= 5) {
+            System.out.println("Your experience level is " + " " + Middle);
+        }
+        if (b > 5) {
+            System.out.println("Your experience level is " + " " + Senior);
+        }
+
+        System.out.println("Please enter your current average salary below (digit)");
+
+        int c = Integer.parseInt(new Scanner(System.in).next());
+
+        if (c <= 1000) {
+            System.out.println("You're sooo looser! Muahahahahaha)))");
+        }
+        if (c > 1001 && c <= 1999) {
+            System.out.println("Your current average salary now is lower than it should be");
+        } else if (c > 1999 && c <= 1000000) {
+            System.out.println("Your current average salary now is just about right");
+        }
+        System.out.println("Have a nice day!");
+
+        Random random = new Random();
+        System.out.println(random.ints(4,5,7));
+    }
+
+}
+
+
+
+
